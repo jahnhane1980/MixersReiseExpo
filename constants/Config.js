ea651bc3e02d55a06dee13ef19a99a9b5c901c0f
jahnhane1980 <jahnhane@gmail.com>
2026-03-31 10:25:42 +0200

    mod verringer need min/max

--- constants/Config.js
+++ constants/Config.js
@@ -7,14 +7,14 @@ export const Config = {
   NIGHT_MODE_START: '22:00',  //Test '08:00', 
   NIGHT_MODE_END: '06:00',   //Test '10:00',   
 
-  NEED_CONFIG: {
+NEED_CONFIG: {
     FULL_POINTS_UNTIL: 15 * 60 * 1000,  // 15 Min: Volle Belohnung
     DECAY_UNTIL: 60 * 60 * 1000,       // Bis 60 Min: Sinkende Belohnung
     PENALTY_AFTER: 120 * 60 * 1000,    // Nach 120 Min: Punktabzug
     MIN_MULTIPLIER: 0.2,               
     
-    // Intervalle für den produktiven Betrieb, Test jeweils 5000 eintragen 
-    NEXT_NEED_DELAY_MIN: 1 * 60 * 60 * 1000, // 1 Std.
-    NEXT_NEED_DELAY_MAX: 5 * 60 * 60 * 1000  // 5 Std.
+    // Neue Intervalle: Min 20 Min, Max 1 Std.
+    NEXT_NEED_DELAY_MIN: 20 * 60 * 1000, // 20 Min.
+    NEXT_NEED_DELAY_MAX: 60 * 60 * 1000   // 1 Std.
   }
 };
